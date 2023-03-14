d09fd9dc84415abb004668fcce49599b6c2efcdd
MatheusThiesen <matheusreis314@gmail.com>
2023-03-14 20:21:34 -0300

    rota teste publica

--- src/modules/home/home.controller.ts
+++ src/modules/home/home.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from 'src/common/decorators';
 
 @Controller('/')
 export class HomeController {
+  @Public()
   @Get()
   home() {
     const date = new Date().toLocaleString('pt-br', {
