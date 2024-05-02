ff53483d2185fd38e6bde38b2ee21a40df585999
MatheusThiesen <matheusreis314@gmail.com>
2024-05-01 23:05:57 -0300

    corrigindo numero de situacao da marca

--- src/modules/app/brands/brands.service.ts
+++ src/modules/app/brands/brands.service.ts
@@ -114,7 +114,7 @@ export class BrandsService {
       Object.assign(brand, {
         codigo: Number(codigo),
         descricao: descricao,
-        eAtivo: eAtivo === 2,
+        eAtivo: Number(eAtivo) === 1,
       });
 
       const brandExists = await this.prisma.marca.findUnique({
